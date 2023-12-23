f0fad5f3776ad281f0281f606aa19d2605c5c566
mnaveen38 <152915762+mnaveen38@users.noreply.github.com>
2023-12-23 14:23:08 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,46 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    mapping(address => uint256) public debtFunds;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event Invest(address indexed investor, uint256 amount);
+    event Redeem(address indexed investor, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +38,34 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    // Function to invest in debt funds
+    function investInDebtFunds(uint256 _investAmount) public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // Perform investment transaction
+        balance -= _investAmount;
+        debtFunds[msg.sender] += _investAmount;
+
+        // Emit the event
+        emit Invest(msg.sender, _investAmount);
+    }
+
+    // Function to redeem from debt funds
+    function redeemFromDebtFunds(uint256 _redeemAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // Ensure sufficient balance in debt funds for redemption
+        require(debtFunds[msg.sender] >= _redeemAmount, "Insufficient balance for redemption");
+
+        // Perform redemption transaction
+        balance += _redeemAmount;
+        debtFunds[msg.sender] -= _redeemAmount;
+
+        // Emit the event
+        emit Redeem(msg.sender, _redeemAmount);
     }
 }
